88ff6f2788ffdbf019f55db350c11f14f0c796a7
Mani <maniharish431@gmail.com>
2025-08-25 22:55:08 +0530

    Jenkins job trigger

---
+++ Python_scripts/Jenkins_test.py
@@ -0,0 +1,17 @@
+import os
+import subprocess
+
+# Define the full path
+file_path = "C:\\Users\\91900\\Documents\\GIT\\Python_scripts\\test.txt"
+dir_path = "C:\\Users\\91900\\Documents\\GIT\\Python_scripts\\"
+
+if os.path.exists(file_path):
+    print("Writing to the file")
+    with open (file_path,"w+") as f:
+        f.write("Summa5")
+        f.seek(0)
+        lines=f.readlines()
+        print(lines)
+
+else:
+    print("File not exists")
